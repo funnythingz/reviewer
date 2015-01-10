3fbce43a7aba24e74ce03eca2b2b0c85f49fd1bd
funnythingz <funnythingz69+github@gmail.com>
2015-01-10 18:36:37 +0900

    rails g model review reviewer:string mail:string comment:text status:string clinic_id:integer action:string

---
+++ db/migrate/20150110093623_create_reviews.rb
@@ -0,0 +1,14 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.string :reviewer
+      t.string :mail
+      t.text :comment
+      t.string :status
+      t.integer :clinic_id
+      t.string :action
+
+      t.timestamps null: false
+    end
+  end
+end
