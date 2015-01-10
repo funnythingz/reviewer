dfc2a60498467215d721186be60224bd754a4632
funnythingz <funnythingz69+github@gmail.com>
2015-01-10 21:08:34 +0900

    added validates, scope and method of model/review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,21 @@
 class Review < ActiveRecord::Base
+
+  validates :reviewer, presence: true
+  validates :mail, presence: true
+  validates :status, presence: true
+  validates :action, presence: true
+  validates :comment, length: { maximum: 1000}, presence: true
+
+  def is_published?
+    status == 'published'
+  end
+
+  def is_draft?
+    status == 'draft'
+  end
+
+  scope :published, -> { where(status: 'published') }
+  scope :draft,     -> { where(status: 'draft') }
+  scope :good,      -> { where(action: 'good') }
+  scope :bad,       -> { where(action: 'bad') }
 end
