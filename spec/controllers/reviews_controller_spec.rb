127087d92b42c2d3daaeb0cb137022325ee1ce32
funnythingz <funnythingz69+github@gmail.com>
2015-01-11 01:06:02 +0900

    modified spec of reviews

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -36,37 +36,6 @@ RSpec.describe ReviewsController, :type => :controller do
   # ReviewsController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
-  describe "GET index" do
-    it "assigns all reviews as @reviews" do
-      review = Review.create! valid_attributes
-      get :index, {}, valid_session
-      expect(assigns(:reviews)).to eq([review])
-    end
-  end
-
-  describe "GET show" do
-    it "assigns the requested review as @review" do
-      review = Review.create! valid_attributes
-      get :show, {:id => review.to_param}, valid_session
-      expect(assigns(:review)).to eq(review)
-    end
-  end
-
-  describe "GET new" do
-    it "assigns a new review as @review" do
-      get :new, {}, valid_session
-      expect(assigns(:review)).to be_a_new(Review)
-    end
-  end
-
-  describe "GET edit" do
-    it "assigns the requested review as @review" do
-      review = Review.create! valid_attributes
-      get :edit, {:id => review.to_param}, valid_session
-      expect(assigns(:review)).to eq(review)
-    end
-  end
-
   describe "POST create" do
     describe "with valid params" do
       it "creates a new Review" do
@@ -97,62 +66,6 @@ RSpec.describe ReviewsController, :type => :controller do
         post :create, {:review => invalid_attributes}, valid_session
         expect(response).to render_template("new")
       end
-    end
-  end
-
-  describe "PUT update" do
-    describe "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested review" do
-        review = Review.create! valid_attributes
-        put :update, {:id => review.to_param, :review => new_attributes}, valid_session
-        review.reload
-        skip("Add assertions for updated state")
-      end
-
-      it "assigns the requested review as @review" do
-        review = Review.create! valid_attributes
-        put :update, {:id => review.to_param, :review => valid_attributes}, valid_session
-        expect(assigns(:review)).to eq(review)
-      end
-
-      it "redirects to the review" do
-        review = Review.create! valid_attributes
-        put :update, {:id => review.to_param, :review => valid_attributes}, valid_session
-        expect(response).to redirect_to(review)
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns the review as @review" do
-        review = Review.create! valid_attributes
-        put :update, {:id => review.to_param, :review => invalid_attributes}, valid_session
-        expect(assigns(:review)).to eq(review)
-      end
-
-      it "re-renders the 'edit' template" do
-        review = Review.create! valid_attributes
-        put :update, {:id => review.to_param, :review => invalid_attributes}, valid_session
-        expect(response).to render_template("edit")
-      end
-    end
-  end
-
-  describe "DELETE destroy" do
-    it "destroys the requested review" do
-      review = Review.create! valid_attributes
-      expect {
-        delete :destroy, {:id => review.to_param}, valid_session
-      }.to change(Review, :count).by(-1)
-    end
-
-    it "redirects to the reviews list" do
-      review = Review.create! valid_attributes
-      delete :destroy, {:id => review.to_param}, valid_session
-      expect(response).to redirect_to(reviews_url)
     end
   end
 
--- spec/views/clinics/show.html.slim_spec.rb
+++ spec/views/clinics/show.html.slim_spec.rb
@@ -4,16 +4,4 @@ RSpec.describe "clinics/show", :type => :view do
   before(:each) do
     @clinic = create(:clinic)
   end
-
-  it "renders attributes in <p>" do
-    render
-    expect(rendered).to match(/Name/)
-    expect(rendered).to match(/Group/)
-    expect(rendered).to match(/Area/)
-    expect(rendered).to match(/Postal code/)
-    expect(rendered).to match(/Address/)
-    expect(rendered).to match(/Tel/)
-    expect(rendered).to match(/Url/)
-    expect(rendered).to match(/Mail/)
-  end
 end
