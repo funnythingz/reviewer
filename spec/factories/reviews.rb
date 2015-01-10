a3ffc5d65bdb294895b4961e5fefaeea0a730a61
funnythingz <funnythingz69+github@gmail.com>
2015-01-10 21:11:34 +0900

    added spec of review

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,11 +1,22 @@
+
+require 'faker'
+
 FactoryGirl.define do
   factory :review do
-    reviewer "MyString"
-    mail "MyString"
-    comment "MyText"
-    status "MyString"
+    reviewer "hrtn"
+    mail Faker::Internet.email
+    status "published"
     clinic_id 1
-    action "MyString"
-  end
+    action "good"
+    comment "けいたん歯科は最高なんだお！"
 
+    factory :review_draft do
+      status "draft"
+    end
+
+    factory :review_bad do
+      action "bad"
+      comment "けいたん歯科はほんとにうんこですね。ぼったくりですね。虫歯どころか全部抜かれやがったんだぜ！どういうことなんだってばよ！"
+    end
+  end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe Review, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'review' do
+
+    before :each do
+      create(:review)
+      create(:review_draft)
+      create(:review_bad)
+    end
+
+    describe 'valid' do
+      let(:review) {create(:review)}
+      it {
+        expect(review).to be_valid
+      }
+    end
+
+    describe 'method' do
+      let(:review) {create(:review)}
+      it {
+        expect(review.is_published?).to be_truthy
+      }
+
+      let(:review_draft) {create(:review_draft)}
+      it {
+        expect(review_draft.is_draft?).to be_truthy
+      }
+    end
+
+    describe 'scope' do
+
+      context 'status' do
+        it {
+          expect(Review.published.first.status).to eq 'published'
+        }
+        it {
+          expect(Review.draft.first.status).to eq 'draft'
+        }
+      end
+
+      context 'action' do
+
+        context 'good' do
+          it {
+            expect(Review.good.first.action).to eq 'good'
+          }
+
+          it {
+            expect(Review.good.first.comment).to eq "けいたん歯科は最高なんだお！"
+          }
+        end
+
+        context 'bad' do
+          it {
+            expect(Review.bad.first.action).to eq 'bad'
+          }
+
+          it {
+            expect(Review.bad.first.comment).to eq "けいたん歯科はほんとにうんこですね。ぼったくりですね。虫歯どころか全部抜かれやがったんだぜ！どういうことなんだってばよ！"
+          }
+        end
+      end
+    end
+  end
 end
