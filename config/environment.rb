c20ec3696474ad8b9b5a3e1aba78ff645a7806cd
funnythingz <funnythingz69+github@gmail.com>
2015-01-10 15:45:42 +0900

    added secrets

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+Rails.application.secrets.secret_key_base = 'c30ed48f0f0bf4db2177fa89184cb97a605040411d2b2cd9cdd14532aca9a6487784c1e80047ffdf8c847b95fcacda74e09e84738d4cb029fcccc468b9e35473'
