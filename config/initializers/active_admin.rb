0635a9b8b2a4f831bc4234f1545da663e6329c09
funnythingz <funnythingz69+github@gmail.com>
2015-01-11 22:34:06 +0900

    config.allow_comments = false

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -6,6 +6,8 @@ ActiveAdmin.setup do |config|
   #
   config.site_title = "Denter Review"
 
+  config.allow_comments = false
+
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
