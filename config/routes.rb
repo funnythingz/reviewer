724a52beb6238dc21c4bad1ca50a1efd96242d14
funnythingz <funnythingz69+github@gmail.com>
2015-01-11 14:14:59 +0900

    removed rails_admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'clinics#index'
 
   devise_for :admin_users
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   get   '/clinics/:id(.:format)', to: 'clinics#show',   as: 'clinic'
   post  '/reviews(.:format)',     to: 'reviews#create', as: 'reviews'
